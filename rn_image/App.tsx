5f043df377d354d66eb3642020f345bc1ef9b8f2
duycuongit <118497439+duycuongit@users.noreply.github.com>
2023-04-02 23:08:03 +0700

    learn image

---
+++ rn_image/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import ImagesExample from './image_example.js'
+
+const App = () => {
+   return (
+      <ImagesExample />
+   )
+}
+export default App
